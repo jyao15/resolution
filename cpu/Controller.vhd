807b556f4df9eb0d6d3edaf65c2f9f4f29f5b881
nndxg <dlhy-521@163.com>
2017-12-07 01:50:21 +0800

    yjn add correct controller

--- cpu/Controller.vhd
+++ cpu/Controller.vhd
@@ -33,8 +33,8 @@ entity Controller is
 	port(	
 		commandIn : in std_logic_vector(15 downto 0);
 		rst : in std_logic;
-		controllerOut :  out std_logic_vector(20 downto 0)
-		-- RegWrite(1) RegDst(3) ReadReg1(3) ReadReg2(1) 
+		controllerOut :  out std_logic_vector(21 downto 0)
+		-- RegWrite(1) RegDst(3) ReadReg1(3) ReadReg2(2) 
 		-- immeSelect(3) ALUSrcB(1) ALUOp(4) 
 		-- MemRead(1) MemWrite(1) MemToReg(1) jump(1) MFPC(1)
 	);
@@ -50,80 +50,77 @@ begin
 		else
 			case commandIn(15 downto 11) is
 				when "00001" =>		--NOP
-					controllerOut <= "000000000000000000000";
+					controllerOut <= "0000000000000000000000";
 				when "00010" =>		--B
-					controllerOut <= "000000001100101000000";
+					controllerOut <= "0000000001100101000000";
 				when "00100" =>		--BEQZ
-					controllerOut <= "000000101010100100000";
+					controllerOut <= "0000001001010100100000";
 				when "00101" =>		--BNEZ
-					controllerOut <= "000000101010101100000";
+					controllerOut <= "0000001001010101100000";
 				when "00110" =>
 					if (commandIn(1 downto 0) = "00") then 	--SLL
-						controllerOut <= "100101000111011000000";
+						controllerOut <= "1001010000111011000000";
 					elsif (commandIn(1 downto 0) = "11") then --SRA
-						controllerOut <= "100101000111011100000";
+						controllerOut <= "1001010000111011100000";
 					end if;
 				when "01000" =>		--ADDIU3
-					controllerOut <= "101000100011000100000";
+					controllerOut <= "1010001000011000100000";
 				when "01001" =>		--ADDIU
-					controllerOut <= "100100101011000100000";
+					controllerOut <= "1001001001011000100000";
 				when "01100" =>
 					if (commandIn(10 downto 8) = "011") then	 --ADDSP
-						controllerOut <= "110110001011000100000";
+						controllerOut <= "1101100001011000100000";
 					elsif (commandIn(10 downto 8) = "000") then--BTEQZ
-						controllerOut <= "000001101010100100000";
+						controllerOut <= "0000011001010100100000";
+					elsif (commandIn(10 downto 8) = "001") then--BTNEZ
+						controllerOut <= "0000011001010101100000";
 					elsif (commandIn(10 downto 8) = "100") then--MTSP
-						controllerOut <= "110101000000111000000";
+						controllerOut <= "1101010000000111000000";
 					end if;
 				when "01101" =>		--LI
-					controllerOut <= "100100001001111100000";
-				when "01110" =>		--CMPI
-					controllerOut <= "110000101011100000000";
+					controllerOut <= "1001000001001111100000";
 				when "01111" =>		--MOVE
-					controllerOut <= "100101000000111000000";
+					controllerOut <= "1001010000000111000000";
 				when "10010" =>		--LW_SP
-					controllerOut <= "100110001011000110100";
+					controllerOut <= "1001100001011000110100";
 				when "10011" =>		--LW
-					controllerOut <= "101000100101000110100";
+					controllerOut <= "1010001000101000110100";
 				when "11010" =>		--SW_SP
-					controllerOut <= "000010001011000101000";
+					controllerOut <= "0000100101011000101000";
 				when "11011" =>		--SW
-					controllerOut <= "000000110101000101000";
+					controllerOut <= "0000001110101000101000";
 				when "11100" =>
 					if (commandIn(1 downto 0) = "01") then		--ADDU
-						controllerOut <= "101100110000000100000";
+						controllerOut <= "1011001110000000100000";
 					elsif (commandIn(1 downto 0) = "11") then --SUBU
-						controllerOut <= "101100110000001000000";
+						controllerOut <= "1011001110000001000000";
 					end if;
 				when "11101" =>
 					if (commandIn(4 downto 0) = "01100") then		--AND
-						controllerOut <= "100100110000001100000";
+						controllerOut <= "1001001110000001100000";
 					elsif (commandIn(4 downto 0) = "01101") then --OR
-						controllerOut <= "100100110000010000000";
+						controllerOut <= "1001001110000010000000";
 					elsif (commandIn(4 downto 0) = "01010") then --CMP
-						controllerOut <= "110000110000100000000";
-					elsif (commandIn(4 downto 0) = "00100") then --SLLV
-						controllerOut <= "101001000000110000000";
-					elsif (commandIn(4 downto 0) = "00111") then --SRAV
-						controllerOut <= "101001000000110100000";
-					elsif (commandIn(4 downto 0) = "01011") then --NEG
-						controllerOut <= "100101000000010100000";
+						controllerOut <= "1100001110000100000000";
+					elsif (commandIn(4 downto 0) = "00110") then --SRLV
+						controllerOut <= "1010010100000110000000";
+					elsif (commandIn(4 downto 0) = "01111") then --NOT
+						controllerOut <= "1001010000000010100000";
 					elsif (commandIn(7 downto 0) = "00000000") then --JR
-						controllerOut <= "000000100000000000010";
+						controllerOut <= "0000001000000000000010";
 					elsif (commandIn(7 downto 0) = "01000000") then --MFPC
-						controllerOut <= "100100000000000000001";
+						controllerOut <= "1001000000000000000001";
 					end if;
 				when "11110" =>
 					if (commandIn(7 downto 0) = "00000000") then 	--MFIH
-						controllerOut <= "100110100000111000000";
+						controllerOut <= "1001101000000111000000";
 					elsif (commandIn(7 downto 0) = "00000001") then --MTIH
-						controllerOut <= "111000100000111000000";
+						controllerOut <= "1110001000000111000000";
 					end if;
 				when others =>			--Error
-					controllerOut <= "000000000000000000000";
+					controllerOut <= "0000000000000000000000";
 			end case;
 		end if;
 	end process;
 		
 end Behavioral;
-
